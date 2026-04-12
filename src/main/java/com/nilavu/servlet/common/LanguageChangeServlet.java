479f5a59f793f480b3303a3bea61622071cfd0c2
SURYA A R <SURYA A R@192.168.1.33>
2026-04-12 14:20:36 +0530

    feat: Added language switching for user . Supports English, Malayalam and Tamil

---
+++ src/main/java/com/nilavu/servlet/common/LanguageChangeServlet.java
@@ -0,0 +1,33 @@
+package com.nilavu.servlet.common;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/change-language")
+public class LanguageChangeServlet extends HttpServlet{
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException{
+			
+			String language = request.getParameter("lang");
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("lang", language);
+			
+			String referer = request.getHeader("referer");
+			
+			if(referer != null) {
+				response.sendRedirect(referer);
+			}
+			
+			else {
+				response.sendRedirect(request.getContextPath() + "/user/home.jsp");
+			}
+	}
+}
