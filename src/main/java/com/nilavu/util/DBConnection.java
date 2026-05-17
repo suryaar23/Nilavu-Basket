08c4094773d0f4f83d817c6053c2477810402eb9
SURYA A R <152587905+suryaar23@users.noreply.github.com>
2026-05-17 12:38:11 +0530

    Hide database password in DBConnection.java
    Obscured the database password for security reasons.

--- src/main/java/com/nilavu/util/DBConnection.java
+++ src/main/java/com/nilavu/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     // CHANGE these according to your MySQL setup
     private static final String URL = "jdbc:mysql://localhost:3306/nilavu_basket";
     private static final String USER = "root";
-    private static final String PASSWORD = "AuraSphere"; // change to your MySQL password
+    private static final String PASSWORD = "******"; // change to your MySQL password
 
     public static Connection getConnection() throws Exception {
         // Load MySQL driver
